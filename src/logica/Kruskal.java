2d97ee41b2ef892ae37cbe93d3c39c1e2676be9c
FranciscoJulian32 <franciscojulian32@outlook.com>
2024-05-06 23:48:31 -0300

    se realizaron cambios en logica

---
+++ src/logica/Kruskal.java
@@ -0,0 +1,20 @@
+package logica;
+
+import java.util.*;
+import construccion.Arista;
+import construccion.Vertice;
+import construccion.Grafo;
+
+public class Kruskal {
+	
+	private ArrayList<Arista> aristas = new ArrayList<>();
+    private ArrayList<HashSet<Vertice>> vertices = new ArrayList<>();
+    private Grafo grafo;
+
+	public Kruskal() {
+		// TODO Auto-generated constructor stub
+		this.grafo=grafo;
+		this.aristas= aristas; 
+	}
+
+}
